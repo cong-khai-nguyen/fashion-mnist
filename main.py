8e9c759f4d80249b50f09615f4c5023ed1bbb9a6
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-03-14 16:28:39 -0500

    set up the three layers of neural network

--- main.py
+++ main.py
@@ -3,4 +3,31 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = keras.datasets.fashion_mnist
+df = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = df.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images/255.0
+test_images = test_images/255.0
+
+# print(train_images[0], train_labels[0])
+#
+# plt.gray()
+# plt.imshow(train_images[7], cmap=plt.cm.binary)
+# plt.show()
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape = (28,28)),
+    keras.layers.Dense(128, activation = "relu"),
+    keras.layers.Dense(10, activation = "softmax")
+])
+
+model.compile(optimizer = "adam", loss = "sparse_categorical_crossentropy", metrics = ["accuracy"])
+
+model.fit(train_images, train_labels, epochs = 10)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print("Test Accuracy:", test_acc)
